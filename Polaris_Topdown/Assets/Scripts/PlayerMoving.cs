1105262373dd17845a223282e37b294a5fc3427e
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-08-02 19:04:17 +0300

    bind animation and move

--- Polaris_Topdown/Assets/Scripts/PlayerMoving.cs
+++ Polaris_Topdown/Assets/Scripts/PlayerMoving.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMoving : MonoBehaviour
 {
     public float Speed = 10f;
     private Rigidbody _rb;
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,10 +23,14 @@ public class PlayerMoving : MonoBehaviour
 
         float moveVertical = Input.GetAxis("Vertical");
 
-        float accelaration = Input.GetAxis("Accelaration");
+        float accelaration = Input.GetAxis("Acceleration") == 0 ? 1 : Input.GetAxis("Acceleration");
 
-        Vector3 movement = new Vector3(-moveHorizontal, 0.0f, -moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        _rb.AddForce(movement * Speed);
+        _animator.SetFloat("VerticalSpeed", moveVertical);
+        _animator.SetFloat("HorisontalSpeed", -moveHorizontal);
+        _animator.SetFloat("Acceleration", accelaration);
+
+        _rb.AddForce(movement * Speed * accelaration);
     }
 }
