86e284d96a3f3566be33369121416ab0822bb537
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-08-06 12:18:30 +0300

    Create rotation on the mouse

--- Assets/Scripts/PlayerMoving.cs
+++ Assets/Scripts/PlayerMoving.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerStats))]
 public class PlayerMoving : MonoBehaviour
 {
+    [SerializeField] private GameObject test;
     [SerializeField] private float Speed = 10f;
     [SerializeField] private float RotationSpeed = 3f;
     //Понижающий коэфициент при беге назад
@@ -32,28 +33,37 @@ public class PlayerMoving : MonoBehaviour
         float accelaration = Input.GetAxis("Acceleration");
 
         Rotation(moveHorizontal);
-        Move(moveVertical, accelaration);
+        Move(moveVertical, moveHorizontal, accelaration);
     }
 
     private void Rotation(float moveHorizontal)
     {
-        Quaternion quaternion =
-            Quaternion.Euler(new Vector3(0, moveHorizontal * RotationSpeed, 0) + _rb.rotation.eulerAngles);
+        //Quaternion quaternion =
+        //    Quaternion.Euler(new Vector3(0, moveHorizontal * RotationSpeed, 0) + _rb.rotation.eulerAngles);
 
-        _rb.MoveRotation(quaternion);
+        //_rb.MoveRotation(quaternion);
+
+        Vector3 Mpos = Input.mousePosition;
+        Vector3 pos = Camera.main.ScreenPointToRay(Mpos).direction * 12;
+        pos.y = transform.position.y;
+        Vector3 coordinate = new Vector3(Camera.main.transform.position.x, 0, Camera.main.transform.position.z) + pos;
+        test.transform.position = coordinate;
+        transform.LookAt(coordinate);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(coordinate), RotationSpeed * Time.deltaTime);
     }
 
-    private void Move(float moveVertical, float accelaration)
+    private void Move(float moveVertical, float moveHorizontal, float accelaration)
     {
         moveVertical = moveVertical > 0 ? moveVertical : moveVertical / RunBackReductionCoef;
+        moveHorizontal = moveHorizontal / RunBackReductionCoef;
 
-        Vector3 movement = new Vector3(0f, 0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
 
-        if (accelaration < 1)
+        if (accelaration < 1 || movement.x != 0 || movement.z <= 0)
         {
             accelaration = 1;
             //Востановление стамины каждый кадр без бега
-            Stats.Stamina += StaminaCostPerFrame/10;
+            Stats.Stamina += StaminaCostPerFrame / 10;
         }
         else
         {
