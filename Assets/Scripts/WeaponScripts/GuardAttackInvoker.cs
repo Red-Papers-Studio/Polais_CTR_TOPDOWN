8748ffe60793fc0a8c3a69591ba024eaf9ced969
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-08-12 12:59:00 +0300

    Guard random attack

--- Assets/Scripts/WeaponScripts/GuardAttackInvoker.cs
+++ Assets/Scripts/WeaponScripts/GuardAttackInvoker.cs
@@ -1,14 +1,23 @@
-using System;
+using UnityEngine;
+using Random = System.Random;
 public class GuardAttackInvoker : AttackInvoker
 {
+    // 0 - SimpleAttack, 1 - HightAttack
+    private int _attackNumber;
+    private Random _random = new Random(); // ебаный рондом выдает одно и то же число
     public override void Attack()
     {
-        _animator.SetBool("SimpleAttack", true);
+        _attackNumber = _random.Next(0, 2);
+        Debug.Log(_attackNumber);
+        if (_attackNumber == 0) _animator.SetBool("SimpleAttack", true);
+        else _animator.SetBool("HightAttack", true);
     }
 
     protected override void EndAttackAnimation()
     {
-        _animator.SetBool("SimpleAttack", false);
+        if (_attackNumber == 0) _animator.SetBool("SimpleAttack", false);
+        else _animator.SetBool("HightAttack", false);
         base.EndAttackAnimation();
     }
+
 }
