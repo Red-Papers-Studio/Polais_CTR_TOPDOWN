ef9b533d03fb155fcb7fcf8f709e5cc4745e0235
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-08-12 17:05:25 +0300

    fix guard attack random

--- Assets/Scripts/WeaponScripts/GuardAttackInvoker.cs
+++ Assets/Scripts/WeaponScripts/GuardAttackInvoker.cs
@@ -7,15 +7,15 @@ public class GuardAttackInvoker : AttackInvoker
     private Random _random = new Random(); // ебаный рондом выдает одно и то же число
     public override void Attack()
     {
-        _attackNumber = _random.Next(0, 2);
+        _attackNumber = _random.Next(0, 100);
         Debug.Log(_attackNumber);
-        if (_attackNumber == 0) _animator.SetBool("SimpleAttack", true);
+        if (_attackNumber <= 70) _animator.SetBool("SimpleAttack", true);
         else _animator.SetBool("HightAttack", true);
     }
 
     protected override void EndAttackAnimation()
     {
-        if (_attackNumber == 0) _animator.SetBool("SimpleAttack", false);
+        if (_attackNumber <= 70) _animator.SetBool("SimpleAttack", false);
         else _animator.SetBool("HightAttack", false);
         base.EndAttackAnimation();
     }
