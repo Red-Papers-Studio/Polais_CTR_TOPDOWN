0655f474bc7234d1137c0b18f6da4c65ecc7112b
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-09-20 21:59:43 +0300

    Text show update

--- Assets/Scripts/GameInterfaceScripts/TipShower.cs
+++ Assets/Scripts/GameInterfaceScripts/TipShower.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TipShower : MonoBehaviour
 {
     [SerializeField] GameObject _tipObject;
+    [SerializeField] Text _textObject;
+    private bool _isActive = false;
     void Update()
     {
         if(!(Input.GetAxis("TipShow") < 1))
         {
             _tipObject.SetActive(true);
+            _textObject.text = "WASD или стрелочки - перемещение\n" +
+                           "Space или F - ускорение\n" +
+                           "Q - смена оружия\n" +
+                           "ЛКМ - Атака";
+
+            _isActive = true;
         }
         else
         {
-            _tipObject.SetActive(false);
+            if(!_isActive)
+            {
+                _textObject.text = "Нажмите на TAB для подсказки.";
+            }
+            else
+            {
+                _tipObject.SetActive(false);
+            }    
         }
     }
 }
