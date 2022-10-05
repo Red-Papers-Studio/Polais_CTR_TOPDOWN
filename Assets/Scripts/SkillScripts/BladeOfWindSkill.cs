3902263687b618dd73ba67554b8d66d541bae8ef
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-10-05 18:44:47 +0300

    Finish wind skill class

---
+++ Assets/Scripts/SkillScripts/BladeOfWindSkill.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.SkillScripts
+{
+    public class BladeOfWindSkill : Skill
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private Transform _spawnTransform;
+        [SerializeField] private GameObject _windObject;
+        [SerializeField] private SkillData _skillData;
+        [SerializeField] private float _windSpeed;
+
+        private void Start()
+        {
+            _windObject.SetActive(false);
+        }
+
+        protected override void AttackAction(SkillData skillData)
+        {
+            _windObject.SetActive(true);
+            _windObject.transform.position = _target.position;
+            _windObject.transform.rotation = _target.rotation;
+            var wind = Instantiate(_windObject, _spawnTransform.position, _spawnTransform.rotation);
+            wind.GetComponent<Rigidbody>().velocity = _spawnTransform.transform.forward * _windSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/SkillScripts/WindObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.SkillScripts
+{
+    public class WindObject : MonoBehaviour
+    {
+        [SerializeField]
+        private SkillData _skillData;
+        [SerializeField]
+        private float lifeTime;
+        private Collider _arrowCollider;
+        [SerializeField]
+        private float _windAttackDistance = 0;
+        private float _distanceToTarget;
+
+        private void Start()
+        {
+            _arrowCollider = GetComponent<Collider>();
+        }
+
+        private void FixedUpdate()
+        {
+            if(_windAttackDistance >= _distanceToTarget)
+            {
+                Destroy(gameObject);
+            }
+        }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.TryGetComponent<IDamageable>(out IDamageable target))
+            {
+                target.Damage(_skillData.Damage);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
