1961aabeda05a42fe40304d8bc5148763e93eafc
Ivan-AI-hub <ivan_grozdny@mail.ru>
2022-10-05 17:53:40 +0300

    Make fireArrowSkill realize

--- Assets/Scripts/SkillScripts/FireArrowSkill.cs
+++ Assets/Scripts/SkillScripts/FireArrowSkill.cs
@@ -1,8 +1,15 @@
 
+using UnityEngine;
+
 public class FireArrowSkill : Skill
 {
+    [SerializeField] private LongRangeWeapon _weapon;
+    [SerializeField] private GameObject _magicArrow;
     protected override void AttackAction(SkillData skillData)
     {
-        
+        var breviousArrow = _weapon.ammoPrefab;
+        _weapon.ammoPrefab = _magicArrow;
+        _weapon.Attack();
+        _weapon.ammoPrefab = breviousArrow;
     }
 }
